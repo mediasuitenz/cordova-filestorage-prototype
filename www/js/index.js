f3d776a2e989df903166194524b54843d51e7e7c
matt-in-a-hat <matt@mediasuite.co.nz>
2015-05-29 15:02:16 +1200

    Update size of image

--- www/js/index.js
+++ www/js/index.js
@@ -12,7 +12,7 @@ $(document).on(ready, function () {
 
   mapLib.init()
 
-  var imageUrl = 'http://api.tiles.mapbox.com/v4/mediasuite.kd2jfdae/174.85172,-41.12270,13/155x102.png?access_token=pk.eyJ1IjoibWVkaWFzdWl0ZSIsImEiOiJ0LXVxaXRZIn0.mojcG58Ox951Pumx0UdJ_g'
+  var imageUrl = 'http://api.tiles.mapbox.com/v4/mediasuite.kd2jfdae/174.85172,-41.12270,13/1280x1020.png?access_token=pk.eyJ1IjoibWVkaWFzdWl0ZSIsImEiOiJ0LXVxaXRZIn0.mojcG58Ox951Pumx0UdJ_g'
 
   // image.cacheImage(imageUrl, function (err, url) {
   //   imageUrl = url
